d42b3e798e70accaff16706c2ad2bb00e43bf536
Cerlace <124503215+Cerlace@users.noreply.github.com>
2024-11-20 19:57:24 +0300

    Добавил документацию к классу ReflectionUtils

--- Group_Work_1_UniversalDAO/src/main/java/itacademy/utils/ReflectionUtils.java
+++ Group_Work_1_UniversalDAO/src/main/java/itacademy/utils/ReflectionUtils.java
@@ -10,7 +10,19 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
+/**
+ * Утилитарный класс, который содержит методы использующие рефлексию.
+ */
 public class ReflectionUtils {
+    /**
+     * Метод проверят класс на наличие аннотации {@code @TableAnn},
+     * при ее наличии извлекает из нее имя таблицы.
+     * Выбрасывает {@code AnnotationMissingException} в случае отсутствия аннотации.
+     *
+     * @param clazz класс (DTO), ассоциированный с таблицей с помощью аннотаций.
+     * @param <T>   тип DTO, переданный в качестве параметра.
+     * @return строку, содержащую имя таблицы в базе данных.
+     */
     public static <T> String getTableNameByClass(Class<T> clazz) {
         if (clazz.isAnnotationPresent(TableAnn.class)) {
             return clazz.getAnnotation(TableAnn.class).name();
@@ -19,6 +31,17 @@ public class ReflectionUtils {
         }
     }
 
+    /**
+     * Метод проверяет поля класса на наличие аннотации {@code @ColumnAnn},
+     * при ее наличии извлекает из нее имя колонки, а также тип из самого поля.
+     * Полученные данные собираются в упорядоченный список. Также при наличии
+     * аннотации {@code @IdAnn} добавляется строка, обозначающая PRIMARY KEY.
+     *
+     * @param clazz класс (DTO), ассоциированный с таблицей с помощью аннотаций.
+     * @param <T>   тип DTO, переданный в качестве параметра.
+     * @return упорядоченный список, содержащий строки с названиями колонок и их
+     * SQL типами.
+     */
     public static <T> List<String> getColumnNamesAndSqlTypes(Class<T> clazz) {
         List<String> columnNamesAndTypes = new ArrayList<>();
         Field[] fields = clazz.getDeclaredFields();
@@ -39,6 +62,14 @@ public class ReflectionUtils {
         return columnNamesAndTypes;
     }
 
+    /**
+     * Метод проверяет поля объекта на наличие аннотации {@code @IdAnn},
+     * при ее наличии устанавливает переданное значение в соответствующее поле.
+     *
+     * @param t           объект, для которого нужно установить новый id.
+     * @param generatedId новый id.
+     * @param <T>         тип DTO, переданный в качестве параметра.
+     */
     public static <T> void setIdValue(T t, Serializable generatedId) {
         try {
             for (Field field : t.getClass().getDeclaredFields()) {
@@ -53,6 +84,15 @@ public class ReflectionUtils {
         }
     }
 
+    /**
+     * Метод проверяет поля объекта на наличие аннотации {@code @ColumnAnn},
+     * при ее наличии извлекает из нее имя колонки, и добавляет его во множество.
+     *
+     * @param clazz класс (DTO), ассоциированный с таблицей с помощью аннотаций.
+     * @param <T>   тип DTO, переданный в качестве параметра.
+     * @return множество, содержащее набор имен колонок таблицы, соответствующей
+     * переданному классу.
+     */
     public static <T> Set<String> getColumnNames(Class<T> clazz) {
         Set<String> columns = new HashSet<>();
         for (Field field : clazz.getDeclaredFields()) {
@@ -64,6 +104,16 @@ public class ReflectionUtils {
         return columns;
     }
 
+    /**
+     * Метод создает объект переданного типа, и присваивает его полям значения,
+     * переданные через параметр {@code columnsAndValues}.
+     *
+     * @param clazz            класс (DTO), ассоциированный с таблицей с помощью аннотаций.
+     * @param <T>              тип DTO, переданный в качестве параметра.
+     * @param columnsAndValues набор ключ-значение, содержащий имена колонок в качестве ключа,
+     *                         и значения этих колонок из таблицы.
+     * @return объект типа {@code T}, с установленными значениями.
+     */
     public static <T> T buildObject(Class<T> clazz, Map<String, Object> columnsAndValues) {
         try {
             T object = clazz.getDeclaredConstructor().newInstance();
