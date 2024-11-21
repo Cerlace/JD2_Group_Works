da06e7bf49223aa0cd9d4812cad1684882925ad8
Cerlace <124503215+Cerlace@users.noreply.github.com>
2024-11-21 16:42:25 +0300

    Добавил аннотацию @FunctionalInterface в SQLExecutor

--- Group_Work_1_UniversalDAO/src/main/java/itacademy/api/SQLExecutor.java
+++ Group_Work_1_UniversalDAO/src/main/java/itacademy/api/SQLExecutor.java
@@ -3,6 +3,7 @@ package itacademy.api;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public interface SQLExecutor<T>{
+@FunctionalInterface
+public interface SQLExecutor<T> {
     T execute(Connection connection) throws SQLException;
 }
