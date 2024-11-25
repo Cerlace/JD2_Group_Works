19b374e6c6b13080bb2d219f01f7415b42cc6c64
Uladzimir Shunevich <ksiprus@mail.ru>
2024-11-25 09:03:25 +0300

    - создан класс для вывода данных в виде таблицы в консоли

---
+++ Group_Work_1_UniversalDAO/src/main/java/itacademy/utils/DataPrinter.java
@@ -0,0 +1,123 @@
+package itacademy.utils;
+
+import itacademy.dto.Address;
+import itacademy.dto.People;
+import java.util.List;
+
+/**
+ * Класс DataPrinter отвечает за вывод информации о адресах и людях.
+ *
+ * <p>Этот класс предоставляет методы для вывода одной записи
+ * или всех записей о {@link Address} и {@link People} в консоль.</p>
+ */
+public class DataPrinter {
+
+    /**
+     * Выводит информацию об одном адресе.
+     *
+     * @param address объект адреса для отображения
+     */
+    public void oneAddressPrint(Address address) {
+        if (address != null) {
+            printAddressHeader(); // Вывод заголовка таблицы адресов
+            printAddressRow(address); // Вывод строки с данными об адресе
+        } else {
+            System.out.println("Адрес не предоставлен."); // Сообщение при отсутствии данных
+        }
+        System.out.println(); // Пустая строка для отделения выводов
+    }
+
+    /**
+     * Выводит информацию о всех адресах.
+     *
+     * @param addresses список адресов для отображения
+     */
+    public void allAddressPrint(List<Address> addresses) {
+        if (addresses != null && !addresses.isEmpty()) {
+            printAddressHeader(); // Вывод заголовка таблицы адресов
+            for (Address address : addresses) {
+                printAddressRow(address); // Вывод строки для каждого адреса
+            }
+        } else {
+            System.out.println("Нет адресов для вывода."); // Сообщение при отсутствии адресов
+        }
+        System.out.println(); // Пустая строка для отделения выводов
+    }
+
+    /**
+     * Выводит информацию о человеке.
+     *
+     * @param people объект человека для отображения
+     */
+    public void onePeoplePrint(People people) {
+        if (people != null) {
+            printPeopleHeader(); // Вывод заголовка таблицы людей
+            printPeopleRow(people); // Вывод строки с данными о человеке
+        } else {
+            System.out.println("Данные о человеке не предоставлены."); // Сообщение при отсутствии данных
+        }
+        System.out.println(); // Пустая строка для отделения выводов
+    }
+
+    /**
+     * Выводит информацию о всех людях.
+     *
+     * @param peopleList список людей для отображения
+     */
+    public void allPeoplePrint(List<People> peopleList) {
+        if (peopleList != null && !peopleList.isEmpty()) {
+            printPeopleHeader(); // Вывод заголовка таблицы людей
+            for (People people : peopleList) {
+                printPeopleRow(people); // Вывод строки для каждого человека
+            }
+        } else {
+            System.out.println("Нет людей для вывода."); // Сообщение при отсутствии людей
+        }
+        System.out.println(); // Пустая строка для отделения выводов
+    }
+
+    /**
+     * Выводит заголовок таблицы адресов.
+     */
+    private void printAddressHeader() {
+        System.out.printf("+----+--------------------+-------+%n");
+        System.out.printf("| ID | %-18s | %-5s |%n", "Улица", "Дом"); // Заголовки "Улица" и "Дом"
+        System.out.printf("+----+--------------------+-------+%n");
+    }
+
+    /**
+     * Выводит строку с данными об адресе.
+     *
+     * @param address объект адреса для отображения
+     */
+    private void printAddressRow(Address address) {
+        System.out.printf("| %-2d | %-18s | %-5s |%n",
+                address.getId(), // ID адреса
+                address.getStreet(), // Улица
+                address.getHouse()); // Дом
+        System.out.printf("+----+--------------------+-------+%n"); // Завершение строки таблицы
+    }
+
+    /**
+     * Выводит заголовок таблицы людей.
+     */
+    private void printPeopleHeader() {
+        System.out.printf("+----+--------------------+--------------------+----------+%n");
+        System.out.printf("| ID | %-18s | %-18s | %-8s |%n", "Имя", "Фамилия", "Возраст"); // Заголовки "Имя", "Фамилия" и "Возраст"
+        System.out.printf("+----+--------------------+--------------------+----------+%n");
+    }
+
+    /**
+     * Выводит строку с данными о человеке.
+     *
+     * @param people объект человека для отображения
+     */
+    private void printPeopleRow(People people) {
+        System.out.printf("| %-2d | %-18s | %-18s | %-8d |%n",
+                people.getId(), // ID человека
+                people.getName(), // Имя
+                people.getSurname(), // Фамилия
+                people.getAge()); // Возраст
+        System.out.printf("+----+--------------------+--------------------+----------+%n"); // Завершение строки таблицы
+    }
+}
