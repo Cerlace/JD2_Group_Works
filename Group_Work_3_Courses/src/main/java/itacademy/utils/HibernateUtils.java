bd80c5623525d375b47596b08204e78f6b3e5cda
Cerlace <124503215+Cerlace@users.noreply.github.com>
2024-12-13 05:21:03 +0300

    Добавил Logger для HibernateUtils

--- Group_Work_3_Courses/src/main/java/itacademy/utils/HibernateUtils.java
+++ Group_Work_3_Courses/src/main/java/itacademy/utils/HibernateUtils.java
@@ -1,14 +1,25 @@
 package itacademy.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class HibernateUtils {
+    public static final Logger LOGGER = LoggerFactory.getLogger(HibernateUtils.class);
     public static final EntityManagerFactory ENTITY_MANAGER_FACTORY =
             Persistence.createEntityManagerFactory("academy");
+    public static final String CREATED_NEW_ENTITY_MANAGER = "Created new Entity Manager.";
+    public static final String CLOSING_ENTITY_MANAGER_FACTORY = "Closing Entity Manager Factory.";
 
+    /**
+     * Метод создает новый объект EntityManager
+     * @return новый EntityManager
+     */
     public static EntityManager getEntityManager() {
+        LOGGER.info(CREATED_NEW_ENTITY_MANAGER);
         return ENTITY_MANAGER_FACTORY.createEntityManager();
     }
 
@@ -16,6 +27,7 @@ public class HibernateUtils {
      * Метод закрывает EntityManagerFactory
      */
     public static void close() {
+        LOGGER.info(CLOSING_ENTITY_MANAGER_FACTORY);
         ENTITY_MANAGER_FACTORY.close();
     }
 }
