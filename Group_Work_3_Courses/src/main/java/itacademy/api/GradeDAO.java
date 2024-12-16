2c632175eb2c0720e01ede000f6c9e178aea1756
Cerlace <124503215+Cerlace@users.noreply.github.com>
2024-12-16 17:24:30 +0300

    Checkstyle исправления

--- Group_Work_3_Courses/src/main/java/itacademy/api/GradeDAO.java
+++ Group_Work_3_Courses/src/main/java/itacademy/api/GradeDAO.java
@@ -7,5 +7,11 @@ import itacademy.entity.Student;
 import java.util.Set;
 
 public interface GradeDAO extends IDAO<Grade> {
+    /**
+     * Выбирает все оценки {@code Grade} студента, полученные за курс
+     * @param course курс, за который выставлены оценки
+     * @param student студент, оценки которого необходимо получить
+     * @return множество оценок, соответствующих условию
+     */
     Set<Grade> getStudentsGradesByCourse(Course course, Student student);
 }
--- Group_Work_3_Courses/src/main/java/itacademy/api/StudentDAO.java
+++ Group_Work_3_Courses/src/main/java/itacademy/api/StudentDAO.java
@@ -5,5 +5,11 @@ import itacademy.entity.Student;
 import java.util.Set;
 
 public interface StudentDAO extends IDAO<Student> {
+    /**
+     * Выбирает всех студентов, средняя оценка которых больше или равна
+     * заданному значению
+     * @param minAvgGrade минимальная средняя оценка для выбора
+     * @return множество студентов, соответствующих условию
+     */
     Set<Student> getStudentsByMinAvgGrade(Integer minAvgGrade);
 }
