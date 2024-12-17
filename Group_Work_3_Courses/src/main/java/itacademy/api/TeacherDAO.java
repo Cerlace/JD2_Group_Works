8593cc9accc61edde335eb2833d3603c68965488
Cerlace <124503215+Cerlace@users.noreply.github.com>
2024-12-17 05:49:42 +0300

    Реализовал дополнительные методы для TeacherDAO

--- Group_Work_3_Courses/src/main/java/itacademy/api/TeacherDAO.java
+++ Group_Work_3_Courses/src/main/java/itacademy/api/TeacherDAO.java
@@ -2,5 +2,23 @@ package itacademy.api;
 
 import itacademy.entity.Teacher;
 
+import java.util.List;
+
 public interface TeacherDAO extends IDAO<Teacher> {
+    /**
+     * Выбирает учителя {@code Teacher}, по курсам {@code Course}  которого задано больше
+     * всего заданий {@code Task}
+     *
+     * @return учитель {@code Teacher}, соответствующий условию
+     */
+    Teacher getTeacherWithGreatestTaskCount();
+
+    /**
+     * Выбирает нескольких учителей {@code Teacher}, с самым низким
+     * количеством курсов {@code Course}
+     *
+     * @param count количество учителей для выбора
+     * @return список учителей {@code List<Teacher>}, по условию выбора
+     */
+    List<Teacher> getTeachersWithLeastCoursesCount(int count);
 }
--- Group_Work_3_Courses/src/main/java/itacademy/dao/impl/TeacherDAOImpl.java
+++ Group_Work_3_Courses/src/main/java/itacademy/dao/impl/TeacherDAOImpl.java
@@ -3,13 +3,47 @@ package itacademy.dao.impl;
 import itacademy.api.TeacherDAO;
 import itacademy.dao.DAO;
 import itacademy.entity.Teacher;
+import itacademy.utils.ExecutorUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
 public class TeacherDAOImpl extends DAO<Teacher> implements TeacherDAO {
     private static final Logger LOGGER = LoggerFactory.getLogger(TeacherDAOImpl.class);
+    private static final String SELECT_ONE_LOG_MESSAGE = "Table: '{}', starting select single result query";
+    private static final String SELECT_MANY_LOG_MESSAGE = "Table: '{}', starting select many results query";
+    private static final String SELECT_TEACHER_WITH_GREATEST_TASK_COUNT_QUERY =
+            "SELECT teacher FROM Teacher teacher JOIN teacher.courses course JOIN course.tasks task " +
+                    "GROUP BY teacher ORDER BY COUNT(task) DESC";
+    private static final String SELECT_TEACHERS_WITH_LEAST_COURSES_COUNT_QUERY =
+            "SELECT t FROM Teacher t ORDER BY t.courses.size";
+    private final EntityManager em;
 
     public TeacherDAOImpl() {
         super(Teacher.class, LOGGER);
+        this.em = super.getEntityManager();
+    }
+
+    @Override
+    public Teacher getTeacherWithGreatestTaskCount() {
+        return ExecutorUtils.executeHibernate(this.em, em -> {
+            LOGGER.info(SELECT_ONE_LOG_MESSAGE, super.getTableName());
+            TypedQuery<Teacher> query = em.createQuery(SELECT_TEACHER_WITH_GREATEST_TASK_COUNT_QUERY, Teacher.class);
+            query.setMaxResults(1);
+            return query.getSingleResult();
+        });
+    }
+
+    @Override
+    public List<Teacher> getTeachersWithLeastCoursesCount(int count) {
+        return ExecutorUtils.executeHibernate(this.em, em -> {
+            LOGGER.info(SELECT_MANY_LOG_MESSAGE, super.getTableName());
+            TypedQuery<Teacher> query = em.createQuery(SELECT_TEACHERS_WITH_LEAST_COURSES_COUNT_QUERY, Teacher.class);
+            query.setMaxResults(count);
+            return query.getResultList();
+        });
     }
 }
