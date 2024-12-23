68b1de8a76b6b0e66dac4b1ee6c891f3cb87b15c
Simon35845 <DrakeKingman1991@gmail.com>
2024-12-23 23:12:41 +0300

    DAO - убрал неиспользуемый метод get() CarDAO - убрал коммент TODO (CarEntity уже написан)

--- Group_Work_4_CarServlet/src/main/java/itacademy/dao/CarDAO.java
+++ Group_Work_4_CarServlet/src/main/java/itacademy/dao/CarDAO.java
@@ -2,6 +2,6 @@ package itacademy.dao;
 
 import itacademy.entity.CarEntity;
 
-public interface CarDAO extends DAO<CarEntity> { //TODO создать  CarEntity и подставить его тут
+public interface CarDAO extends DAO<CarEntity> {
 
 }
