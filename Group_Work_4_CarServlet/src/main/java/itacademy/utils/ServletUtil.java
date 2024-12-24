f3dee91a1fbe0882b5c6652ff5d073af1bba9f3a
Roman Gulyako <romangulyako@gmail.com>
2024-12-24 11:09:02 +0300

    Добавил документацию для метода ServletUtil

--- Group_Work_4_CarServlet/src/main/java/itacademy/utils/ServletUtil.java
+++ Group_Work_4_CarServlet/src/main/java/itacademy/utils/ServletUtil.java
@@ -6,6 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 public class ServletUtil {
+    /**
+     * Получает из объекта HttpServletRequest параметр по его имени
+     * @param req объект HttpServletRequest
+     * @param nameField имя параметра, который получаем из HttpServletRequest
+     * @return строковое представление параметра
+     */
     public static String getParam(HttpServletRequest req, String nameField) {
         return Optional.ofNullable(req.getParameter(nameField))
                 .filter(StringUtils::isNotBlank)
