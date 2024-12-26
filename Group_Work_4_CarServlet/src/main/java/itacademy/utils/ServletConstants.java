09d06a57d4913797edd5d90f39f96b6826f0c218
Cerlace <124503215+Cerlace@users.noreply.github.com>
2024-12-26 17:34:54 +0300

    Написал класс с константами для сервлетов

---
+++ Group_Work_4_CarServlet/src/main/java/itacademy/utils/ServletConstants.java
@@ -0,0 +1,10 @@
+package itacademy.utils;
+
+public class ServletConstants {
+    public static final String CARS_LIST_SERVLET = "cars_manager";
+    public static final String CARS_LIST_JSP = "/cars_manager.jsp";
+    public static final String CARS_LIST_ATTRIBUTE = "cars";
+    public static final String CAR_ID_PARAMETER = "id";
+    public static final String CAR_VIN_PARAMETER = "vin";
+    public static final String CAR_NAME_PARAMETER = "name";
+}
