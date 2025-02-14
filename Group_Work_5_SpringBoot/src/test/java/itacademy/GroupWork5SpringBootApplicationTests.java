7ef2ca42c92910ee0cfb726e2d70c7df6648bb9b
Roman Gulyako <romangulyako@gmail.com>
2025-02-14 16:27:09 +0300

    Создал проект с нужными зависимостями
    Настроил application.properties
    В .gitignore добавил нужные паттерны

---
+++ Group_Work_5_SpringBoot/src/test/java/itacademy/GroupWork5SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package itacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupWork5SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
